2efe6556c129dce01c61234de4927254624b88c3
Datseris <datseris.george@gmail.com>
2024-02-11 18:36:26 +0000

    discuss default lhs

--- src/API.jl
+++ src/API.jl
@@ -6,12 +6,13 @@ A process subtype `p::Process` extends the functions:
 - `rhs(p)` which is the right-hand-side expression, i.e., the "actual" process.
 - (optional) `timescale`, which defaults to [`NoTimeDerivative`](@ref).
 - (optional) `lhs(p)` which returns the left-hand-side. Let `τ = timescale(p)`.
-  Then `lhs(p)` behavior depends on `τ` as follows:
+  Then default `lhs(p)` behavior depends on `τ` as follows:
   - Just `lhs_variable(p)` if `τ == NoTimeDerivative()`.
   - `Differential(t)(p)` if `τ == nothing`.
   - `τ_var*Differential(t)(p)` if `τ isa Union{Real, Num}`. If real,
     a new named parameter `τ_var` is created that has the prefix `:τ_` and then the
     lhs-variable name and has default value `τ`. Else if `Num`, `τ_var = τ` as given.
+  - Explicitly extend `lhs_variable` if the above do not suit you.
 """
 abstract type Process end
 
