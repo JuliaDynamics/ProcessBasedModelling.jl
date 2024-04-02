446c047731096028167a51a33fb938889902ddb5
Datseris <datseris.george@gmail.com>
2024-04-02 11:07:53 +0100

    ensure default dict is stored

--- src/default.jl
+++ src/default.jl
@@ -10,8 +10,9 @@ in the list of registered default processes tracked by the given module.
 If `warn`, throw a warning if a default process with same LHS variable already
 exists and will be overwritten.
 """
-function register_default_process!(process, m::Module; warn = true)
+function register_default_process!(process::Union{Process, Equation}, m::Module; warn = true)
     mdict = get(_DEFAULT_PROCESSES, m, Dict{Num}{Any}())
+    _DEFAULT_PROCESSES[m] = mdict # ensure dict is stored as well
     lhsvar = lhs_variable(process)
     # overwritting here should never happen but oh well.
     if haskey(mdict, lhsvar) && warn
