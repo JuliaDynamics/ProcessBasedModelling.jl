92fef85209d8c381cccada74113b6e5c9c5162e7
George Datseris <datseris.george@gmail.com>
2024-05-07 15:04:40 +0100

    Improve docs a bit more (#9)
    * improve docs significantly
    
    * bump version

--- src/default.jl
+++ src/default.jl
@@ -9,6 +9,8 @@ Register a `process` (`Equation` or `Process`) as a default process for its LHS 
 in the list of default processes tracked by the given module.
 If `warn`, throw a warning if a default process with same LHS variable already
 exists and will be overwritten.
+
+You can use [`default_processes`](@ref) to obtain the list of tracked default processes.
 """
 function register_default_process!(process::Union{Process, Equation}, m::Module; warn = true)
     mdict = default_processes(m)
