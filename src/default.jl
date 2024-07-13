61381cc6100a17691c28381ae25885d6d2631309
Datseris <datseris.george@gmail.com>
2024-07-13 18:29:04 +0200

    add instructions to use the init function

--- src/default.jl
+++ src/default.jl
@@ -11,6 +11,29 @@ If `warn`, throw a warning if a default process with same LHS variable already
 exists and will be overwritten.
 
 You can use [`default_processes`](@ref) to obtain the list of tracked default processes.
+
+!!! note "For developers"
+    If you are developing a new module/package that is based on ProcessBasedModelling.jl,
+    and within it you also register default processes, then enclose your
+    `register_default_process!` calls within the module's `__init__()` function.
+    For example:
+    ```julia
+    module MyProcesses
+    # ...
+
+    function __init__()
+        register_default_process!.(
+            [
+                process1,
+                process2,
+                # ...
+            ],
+            Ref(MyProcesses)
+        )
+    end
+
+    end # module
+    ```
 """
 function register_default_process!(process::Union{Process, Equation}, m::Module; warn = true)
     mdict = default_processes(m)
